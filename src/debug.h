633eea3a1dfb1b5efc8831e2f59a6ef6018b083e
juergen <juergen@kreuzholzen.de>
2010-06-20 17:17:33 +0000

    Even if the DEBUG macro is disabled, all calls to debug_out() where kept. This commit removes them entirely from the binary, if the DEBUG macro is undefined and frees about 2 kiB from the flash memory (mostly the strings).

--- src/debug.h
+++ src/debug.h
@@ -9,7 +9,6 @@
 
 #define DEBUGBAUD                       115200   //uart baud rate: 115200
 #define DEBUGUART                       UART2_BASE //uart: UART1_BASE UART2_BASE
-#define DEBUGOUT                        debug_out
 
 #define COMPILE_YEAR  ((((__DATE__ [7]-'0')*10+(__DATE__[8]-'0'))*10+(__DATE__ [9]-'0'))*10+(__DATE__ [10]-'0'))
 
@@ -25,8 +24,19 @@
 
 #define COMPILE_DAY   ((((__DATE__ [4]==' ')?0:(__DATE__[4]-'0'))*10)+(__DATE__[5]-'0')) 
 
-
 //----- PROTOTYPES -----
+#ifndef __GNUC__
+/* keep old behaviour for non gcc (strict ANSI)  */
+#define DEBUGOUT			debug_out
+#else
+# ifdef DEBUG
+/* this is not strict ANSI, but gcc can handle variable parameter macros */
+#  define DEBUGOUT(x,y...)		debug_out(x, ##y);
+#else
+/* remove corresponding code entirely from the binary */
+#  define DEBUGOUT(x,y...)
+#endif
+#endif
 void                                   debug_out(const char *s, ...);
 void                                   uart_puts(const char *s);
 void                                   uart_putc(unsigned int c);
